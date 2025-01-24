82d7c0855c7097e7d410d7cdecefde61d590a953
Sandić Nenad <sandicnenad81@gmail.com>
2025-01-24 16:57:04 +0100

    35: Completing Repositories

--- HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,8 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Persistance;
 using HR.LeaveManagement.Domain;
 using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace HR.LeaveManagement.Persistence.Repositories
 {
@@ -8,6 +10,34 @@ namespace HR.LeaveManagement.Persistence.Repositories
     {
         public LeaveRequestRepository(HrDatabaseContext context) : base(context)
         {
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequests = await _context.LeaveRequest
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+        {
+            var leaveRequests = await _context.LeaveRequest
+                .Where(q => q.RequestingEmployeeId == userId)
+                .Include(q => q.LeaveType)
+                .ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _context.LeaveRequest
+                .Include(q => q.LeaveType)
+                .FirstOrDefaultAsync(q => q.Id == id);
+
+            return leaveRequest;
         }
     }
 }

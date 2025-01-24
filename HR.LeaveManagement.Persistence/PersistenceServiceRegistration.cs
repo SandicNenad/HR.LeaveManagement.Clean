e72e13e15475a435fcc8a99d305998abae144228
Sandić Nenad <sandicnenad81@gmail.com>
2025-01-24 12:49:16 +0100

    33: Add Repository Implementations

--- HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+﻿using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using HR.LeaveManagement.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,11 @@ namespace HR.LeaveManagement.Persistence
             services.AddDbContext<HrDatabaseContext>(options => {
                 options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
             });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
 
             return services;
         }

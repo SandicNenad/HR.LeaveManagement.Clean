e22ec458807f79723c43dcab4b0308d439f3fbed
Sandić Nenad <sandicnenad81@gmail.com>
2025-01-30 07:59:06 +0100

    48: Part7- Implement ChangeLeaveRequestApproval

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Command/ChangeLeaveRequestApproval/ChangeLeaveRequestApprovalCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Command.ChangeLeaveRequestApproval
+{
+    public class ChangeLeaveRequestApprovalCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+        public bool Approved { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Command/ChangeLeaveRequestApproval/ChangeLeaveRequestApprovalCommandHandler.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
+using HR.LeaveManagement.Application.Contracts.Persistance;
+using HR.LeaveManagement.Application.Exceptions;
+using HR.LeaveManagement.Application.Models.Email;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Command.ChangeLeaveRequestApproval
+{
+    public class ChangeLeaveRequestApprovalCommandHandler : IRequestHandler<ChangeLeaveRequestApprovalCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
+        private readonly IAppLogger<ChangeLeaveRequestApprovalCommandHandler> _logger;
+
+        public ChangeLeaveRequestApprovalCommandHandler(ILeaveRequestRepository leaveRequestRepository,
+            ILeaveTypeRepository leaveTypeRepository, IMapper mapper,
+            IEmailSender emailSender, IAppLogger<ChangeLeaveRequestApprovalCommandHandler> logger)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(ChangeLeaveRequestApprovalCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+            if (leaveRequest is null)
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+
+            leaveRequest.Approved = request.Approved;
+            await _leaveRequestRepository.UpdateAsync(leaveRequest);
+
+            // If request is approved, get and update the emplyee`s allocations
+
+            try
+            {
+                // Send confirmation email
+                var email = new EmailMessage
+                {
+                    To = string.Empty, /* Get email from employee record */
+                    Body = $"Your Leave Request for {leaveRequest.StartDate:D} to {leaveRequest.EndDate:D} has been updated.",
+                    Subject = "Leave Request Approval Status Updated"
+                };
+
+                await _emailSender.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex.Message);
+                throw;
+            }
+
+            return Unit.Value;
+        }
+    }
+}

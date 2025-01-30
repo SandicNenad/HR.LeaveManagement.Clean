8a167b4fd250878283d6980df3e9c32a0637babe
Sandić Nenad <sandicnenad81@gmail.com>
2025-01-30 08:06:09 +0100

    48: Part10- Added LeaveRequestsController , Implemented Get

---
+++ HR.LeaveManagement.Api/Controllers/LeaveRequestsController.cs
@@ -0,0 +1,57 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestDetail;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Queries.GetLeaveRequestList;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class LeaveRequestsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveRequestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<LeaveRequestsController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveRequestListDto>>> Get(bool isLoggedInUser = false)
+        {
+            var leaveRequests = await _mediator.Send(new GetLeaveRequestListQuery());
+            return Ok(leaveRequests);
+        }
+
+        // GET api/<LeaveRequestsController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequestDetailsDto>> Get(int id)
+        {
+            var leaveRequest = await _mediator.Send(new GetLeaveRequestDetailQuery { Id = id });
+            return Ok(leaveRequest);
+        }
+
+        // POST api/<LeaveRequestsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LeaveRequestsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LeaveRequestsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

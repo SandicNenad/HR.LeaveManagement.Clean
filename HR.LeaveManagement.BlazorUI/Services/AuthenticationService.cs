90c55e4ce9742802ca2267801723f60731b055af
Sandić Nenad <sandicnenad81@gmail.com>
2025-02-07 10:14:33 +0100

    68. Add Authentication Services to Blazor - Added AuthentiationService

---
+++ HR.LeaveManagement.BlazorUI/Services/AuthenticationService.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.Services
+{
+    public class AuthenticationService : BaseHttpService, IAuthenticationService
+    {
+        public AuthenticationService(IClient client, ILocalStorageService localSotrage) : base(client, localSotrage)
+        {
+        }
+
+        public Task<bool> AuthenticateAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterAsync(string firstName, string lastName, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

b24e19a29c9e161046932f8c74946e2633fe7385
Sandić Nenad <sandicnenad81@gmail.com>
2025-02-17 21:40:35 +0100

    73: Setup Leave Request Management, Removed/Commented Method AddBearerToken from BaseHttpService

--- HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -43,12 +43,12 @@ namespace HR.LeaveManagement.BlazorUI.Services.Base
             }
         }
 
-        protected async Task AddBearerToken()
-        {
-            if (await _localStorage.ContainKeyAsync("token"))
-                _client.HttpClient.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue("Bearer", await
-                    _localStorage.GetItemAsync<string>("token"));
-        }
+        //protected async Task AddBearerToken()
+        //{
+        //    if (await _localStorage.ContainKeyAsync("token"))
+        //        _client.HttpClient.DefaultRequestHeaders.Authorization =
+        //            new AuthenticationHeaderValue("Bearer", await
+        //            _localStorage.GetItemAsync<string>("token"));
+        //}
     }
 }

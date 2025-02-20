cd21470cb6abb71e09e3ac54112bf46349d9936f
Sandić Nenad <sandicnenad81@gmail.com>
2025-02-20 18:07:49 +0100

    76. Setup Leave Request Management - Modified ServiceClient to allign with API changes

--- HR.LeaveManagement.BlazorUI/Services/Base/ServiceClient.cs
+++ HR.LeaveManagement.BlazorUI/Services/Base/ServiceClient.cs
@@ -2295,6 +2295,9 @@ namespace HR.LeaveManagement.BlazorUI.Services.Base
         [System.Text.Json.Serialization.JsonPropertyName("approved")]
         public bool? Approved { get; set; }
 
+        [System.Text.Json.Serialization.JsonPropertyName("cancelled")]
+        public bool? Cancelled { get; set; }
+
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.2.0.0 (NJsonSchema v11.1.0.0 (Newtonsoft.Json v13.0.0.0))")]

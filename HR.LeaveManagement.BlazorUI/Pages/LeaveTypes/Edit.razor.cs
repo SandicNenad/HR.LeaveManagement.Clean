ef172a60d8cea748c680a808b549f13b558da7ff
Sandić Nenad <sandicnenad81@gmail.com>
2025-02-27 22:03:19 +0100

    82. Section Source Code - Modified some field namings, fixed HrDatabaseContextTests

--- HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Edit.razor.cs
+++ HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Edit.razor.cs
@@ -7,10 +7,10 @@ namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
     public partial class Edit
     {
         [Inject]
-        ILeaveTypeService _client { get; set; }
+        ILeaveTypeService client { get; set; }
 
         [Inject]
-        NavigationManager _navManager { get; set; }
+        NavigationManager navManager { get; set; }
 
         [Parameter]
         public int id { get; set; }
@@ -20,15 +20,15 @@ namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
 
         protected async override Task OnParametersSetAsync()
         {
-            leaveType = await _client.GetLeaveTypeDetails(id);
+            leaveType = await client.GetLeaveTypeDetails(id);
         }
 
         async Task EditLeaveType()
         {
-            var response = await _client.UpdateLeaveType(id, leaveType);
+            var response = await client.UpdateLeaveType(id, leaveType);
             if (response.Success)
             {
-                _navManager.NavigateTo("/leavetypes/");
+                navManager.NavigateTo("/leavetypes/");
             }
             Message = response.Message;
         }
